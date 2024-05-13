9c822c4c7a03b982fd847bcdc692f2b90bbb7638
stefanbgh <stefanbgh.po@gmail.com>
2024-05-13 11:29:06 +0200

    update info

--- src/constants/contact.constant.ts
+++ src/constants/contact.constant.ts
@@ -1,5 +1,4 @@
-const github = "https://github.com/stefan022";
-const linkedin =
-	"https://www.linkedin.com/in/stefan-blagojevi%C4%87-2aa3a5273/";
+const github = "https://github.com/stefanbgh";
+const linkedin = "https://www.linkedin.com/in/stefan-blagojevic-2aa3a5273/";
 
 export { github, linkedin };
